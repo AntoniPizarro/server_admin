c70e7ad1ffe556720e4a77291413c1926b9bdb46
GlobaliaTPG <xx784@globalia.com>
2024-11-21 14:58:50 +0100

    Nuevas funciones generales.

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,4 +1,4 @@
 from .zip_creator import generate_comprimed_file
 from .NBT_funcs import *
 from .minecraft_content import *
-from .funcs import minutes_to_seconds, seconds_to_minutes
+from .funcs import *
--- resources/funcs.py
+++ resources/funcs.py
@@ -3,3 +3,23 @@ def minutes_to_seconds(minutes):
 
 def seconds_to_minutes(seconds):
     return seconds / 60
+
+def fill_string(text: str, width: int) -> list[str]:
+    """
+    Ajusta un texto a un determinado número de caracteres por línea. Separa por palabras
+    """
+    res = []
+    row = ""
+    for word in text.split():
+        if len(row + word) > width:
+            res.append(row[:-1])
+            row = ""
+            if len(word) > width:
+                while len(word) > width:
+                    res.append(word[:width])
+                    word = word[10:]
+        row += word + " "
+    
+    res.append(row)
+    
+    return res
